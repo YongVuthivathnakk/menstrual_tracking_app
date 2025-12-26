cb3b322c38ca9dbdc5410b98f1f9599f9f924134
YongVuthivathnakk <vuthivathnakkyong@gmail.com>
2025-12-26 16:31:48 +0700

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:menstrual_tracking_app/ui/pages/start_page.dart';
+
+class AppPage extends StatelessWidget {
+  const AppPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StartPage();
+  }
+}
